47f856c507926153503f42a7cfc422603d1386f1
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-09-14 15:34:49 +0200

    :construction: work(translations):remove console.log()

--- client/src/locales/fr-FR.ts
+++ client/src/locales/fr-FR.ts
@@ -3,7 +3,6 @@ import axios from 'axios'
 export default async function () {
 	let informations = {}
 	const { data } = await axios.get('http://localhost:3333/api/translations')
-	console.log(data.translations)
 	data.translations.map((item: any) => {
 		const { code, fr } = item
 		informations = { ...informations, [code]: fr }
