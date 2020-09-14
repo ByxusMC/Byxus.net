5fc8e38aa803ba08700ea2f579917ef5ac5a9784
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-09-14 10:54:42 +0200

    :construction: work(users): make errors in  schemas validation

--- server/app/Validators/users/UpdateValidator.ts
+++ server/app/Validators/users/UpdateValidator.ts
@@ -25,6 +25,8 @@ export default class StoreValidator {
 	 */
 	public schema = schema.create({
 		pseudonyme: schema.string.optional({ trim: true }),
+		email: schema.string.optional({ trim: true }, [rules.email(), rules.unique({ table: 'users', column: 'email' })]),
+		password: schema.string.optional({ trim: true }, [rules.confirmed()]),
 		uuid: schema.string.optional({ trim: true }, [
 			rules.regex(/[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/)
 		]),
@@ -57,8 +59,8 @@ export default class StoreValidator {
 	 */
 	public messages = {
 		required: 'Le champ {{ field }} est obligatoire',
-		'firstname.required': 'Le prénom est obligatoire',
-		'lastname.required': 'Le nom est obligatoire',
+		'pseudonyme.required': 'Le pseudo minecarft est obligatoire',
+		'pseudonyme.string': 'Le pseudo doit-être une chaîne de caractère',
 		'email.required': "L'email est obligatoire",
 		'email.email': "L'email doit-être une adresse email valide",
 		'email.unique': "L'email existe déjà, veuillez en choisir une autre",
