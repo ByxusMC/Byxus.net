4330ff70b3326e08d27f96d462e6663ce571bcf6
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-09-16 10:30:15 +0200

    :construction: work(forum): create categories migrations

---
+++ server/database/migrations/1600244950228_categories.ts
@@ -0,0 +1,18 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Categories extends BaseSchema {
+	protected tableName = 'categories'
+
+	public async up() {
+		this.schema.createTable(this.tableName, (table) => {
+			table.increments('id')
+			table.integer('forum_id').unsigned().references('id').inTable('forums')
+			table.integer('label_id').unsigned().references('id').inTable('translations')
+			table.timestamps(true, true)
+		})
+	}
+
+	public async down() {
+		this.schema.dropTable(this.tableName)
+	}
+}
