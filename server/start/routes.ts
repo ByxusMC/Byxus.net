452e58f3e3d7597e1973c6c6ba70801aa1e1e086
Freeze455 <baptiste.parmantier.pro@gmail.com>
2020-09-16 11:25:58 +0200

    :construction: work(forum): update routes

--- server/start/routes.ts
+++ server/start/routes.ts
@@ -23,16 +23,35 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
 	Route.resource('users', 'UsersController').apiOnly().middleware({})
 	Route.resource('roles', 'RolesController').apiOnly().middleware({})
-	Route.resource('translations', 'TranslationsController').apiOnly().middleware({})
-	Route.resource('modules', 'ModulesController').apiOnly().only(['index', 'show', 'update']).middleware({})
+	Route.resource('translations', 'TranslationsController')
+		.apiOnly()
+		.middleware({})
+	Route.resource('modules', 'ModulesController')
+		.apiOnly()
+		.only(['index', 'show', 'update'])
+		.middleware({})
 	Route.resource('forums', 'ForumsController').apiOnly().middleware({})
-	Route.get('/authentication/user/me', 'AuthController.user').middleware('auth')
+	Route.resource('categories', 'CategoriesController')
+		.apiOnly()
+		.middleware({})
+
+	Route.get('/authentication/user/me', 'AuthController.user').middleware(
+		'auth'
+	)
 
 	Route.post('/authentication/api/login', 'AuthController.loginApi')
-	Route.post('/authentication/api/logout', 'AuthController.logoutApi').middleware('auth')
+	Route.post(
+		'/authentication/api/logout',
+		'AuthController.logoutApi'
+	).middleware('auth')
 
 	Route.post('/authentication/web/login', 'AuthController.loginWeb')
-	Route.post('/authentication/web/logout', 'AuthController.logoutWeb').middleware('auth')
+	Route.post(
+		'/authentication/web/logout',
+		'AuthController.logoutWeb'
+	).middleware('auth')
 
-	Route.resource('cookies', 'CookiesController').apiOnly().only(['show', 'store'])
+	Route.resource('cookies', 'CookiesController')
+		.apiOnly()
+		.only(['show', 'store'])
 }).prefix('/api')
